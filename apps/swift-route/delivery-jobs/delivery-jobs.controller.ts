66898df8b6f2fd482a6a58d7beac37470821e0fd
Adriane James Puzon <adrianejames27@gmail.com>
2026-04-25 16:39:34 +0800

    feat(backend): implement static delivery job controller

--- apps/swift-route/delivery-jobs/delivery-jobs.controller.ts
+++ apps/swift-route/delivery-jobs/delivery-jobs.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { deliveryJobsStore } from "../src/stores";
+import { DeliveryJob } from "@swift-route/types";
+import { CreateDeliveryJobModel } from "./dto/create-delivery-job.dto";
 
-@Controller('delivery-jobs')
-export class DeliveryJobsController {}
+@Controller("delivery-jobs")
+export class DeliveryJobsController {
+  // TODO: GET /delivery-jobs
+  @Get()
+  findAll(): DeliveryJob[] {
+    return deliveryJobsStore;
+  }
+  // TODO: GET /delivery-jobs/:id
+  @Get(":id")
+  findOne(@Param("id") id: string) {
+    return deliveryJobsStore.find((record) => record.id === id);
+  }
+  // TODO: POST /delivery-jobs
+  @Post()
+  createOne(@Body() createDeliveryJobModel: CreateDeliveryJobModel) {
+    return createDeliveryJobModel;
+  }
+  // TODO: PATCH /delivery-jobs/:id/status
+}
--- apps/swift-route/src/app.module.ts
+++ apps/swift-route/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DeliveryJobsModule } from '../delivery-jobs/delivery-jobs.module';
 
 @Module({
-  imports: [],
+  imports: [DeliveryJobsModule],
   controllers: [AppController],
   providers: [AppService],
 })
