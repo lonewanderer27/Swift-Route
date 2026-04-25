366243cb998408fbf6c6fbcf18ea0593c94b95be
Adriane James Puzon <adrianejames27@gmail.com>
2026-04-26 01:21:47 +0800

    refactor(mobile): stick to one store for all jobs

--- apps/swift-app/hooks/use-update-delivery-status.tsx
+++ apps/swift-app/hooks/use-update-delivery-status.tsx
@@ -1,18 +1,14 @@
 import DeliveryJobsService from "@/services/delivery-jobs.service";
-import { useDeliveryJobStore } from "@/store/delivery-job.store"
 import { useDeliveryJobsStore } from "@/store/delivery-jobs.store";
 import { DeliveryStatus } from "@swift-route/types";
 
 const useUpdateDeliveryStatus = () => {
   const updateJobStatus = useDeliveryJobsStore((state) => state.advanceJobStatus);
   const revertJobStatus = useDeliveryJobsStore((state) => state.revertJobStatus);
-  const updateJobStatusSI = useDeliveryJobStore((state) => state.advanceJobStatus);
-  const revertJobStatusSI = useDeliveryJobStore((state) => state.revertJobStatus);
 
   const updateStatus = async (id: string, status: DeliveryStatus) => {
-    // optimistically update status in both stores
+    // optimistically update status in store
     updateJobStatus(id, status);
-    updateJobStatusSI(status);
 
     try {
       // call backend to updaate delivery status
@@ -21,7 +17,6 @@ const useUpdateDeliveryStatus = () => {
       // if API call fails, revert status in both stores and set error message
       const errMessage = err instanceof Error ? err.message : "Failed to update delivery status.";
       revertJobStatus(errMessage);
-      revertJobStatusSI(errMessage);
     }
   }
 
