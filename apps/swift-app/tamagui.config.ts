c36e3fb52b974d9a75973d4bba3828967b8e98f7
Adriane James Puzon <adrianejames27@gmail.com>
2026-04-26 21:10:02 +0800

    build(mobile/tamagui): create animations config needed for toast

--- apps/swift-app/tamagui.config.ts
+++ apps/swift-app/tamagui.config.ts
@@ -1,7 +1,17 @@
 import { defaultConfig } from "@tamagui/config/v5";
+import { createAnimations } from "@tamagui/animations-react-native";
 import { createTamagui } from "tamagui";
 
-const config = createTamagui(defaultConfig);
+const animations = createAnimations({
+  bouncy: { type: "spring", damping: 9, mass: 0.9, stiffness: 120 },
+  medium: { type: "spring", damping: 16, stiffness: 90, mass: 0.8 },
+  quick: { type: "spring", damping: 25, mass: 1, stiffness: 550 }
+});
+
+const config = createTamagui({
+  ...defaultConfig,
+  animations,
+});
 
 type Conf = typeof config;
 
