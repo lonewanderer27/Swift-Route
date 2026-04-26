6fa7305fc2cad14efb12971447aae8283cf1fea9
Adriane James Puzon <adrianejames27@gmail.com>
2026-04-26 15:41:13 +0800

    feat(mobile)!: reset project

--- apps/swift-app/hooks/use-delivery-job.tsx
+++
@@ -1,8 +0,0 @@
-import { useDeliveryJobsStore } from "@/store/delivery-jobs.store";
-
-const useDeliveryJob = (jobId: string) => {
-  return useDeliveryJobsStore((state) =>
-    state.jobs.find((job) => job.id === jobId))
-}
-
-export default useDeliveryJob;
