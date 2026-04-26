0836e40e5dd5798a3ed0d60a00513965e0358988
Adriane James Puzon <adrianejames27@gmail.com>
2026-04-26 21:11:11 +0800

    feat(mobile/layout): wrap with toast provider

--- apps/swift-app/app/_layout.tsx
+++ apps/swift-app/app/_layout.tsx
@@ -1,11 +1,13 @@
 import config from "@/tamagui.config";
-import { TamaguiProvider } from 'tamagui'
+import { TamaguiProvider, ToastProvider } from 'tamagui'
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
     <TamaguiProvider config={config} defaultTheme="light">
-      <Stack />
+      <ToastProvider>
+        <Stack />
+      </ToastProvider>
     </TamaguiProvider>
   )
 }
